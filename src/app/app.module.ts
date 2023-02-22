125b51692bf693f2013783324da270d32345db3a
saegben <98625216+saegben@users.noreply.github.com>
2023-02-22 11:37:46 -0600

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PhotosComponent } from './photos/photos.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+
+      { path: 'photos', component: PhotosComponent },
+      /* UPDATE W COMPONENTS */
+      { path: 'audios', component: HomeComponent },
+      { path: 'videos', component: HomeComponent },
+      { path: 'shop', component: HomeComponent },
+
+      { path: '**', redirectTo: 'home', pathMatch: 'full' },
+    ])
   ],
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   bootstrap: [ AppComponent ]
 })
